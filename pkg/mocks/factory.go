e8e293898477b2ef502a75432558938ec8f5ce27
Alexander Matyushentsev <AMatyushentsev@gmail.com>
2021-05-25 12:13:32 -0700

    chore: fake API factory should return error
    Signed-off-by: Alexander Matyushentsev <AMatyushentsev@gmail.com>

--- pkg/mocks/factory.go
+++ pkg/mocks/factory.go
@@ -4,8 +4,9 @@ import "github.com/argoproj/notifications-engine/pkg/api"
 
 type FakeFactory struct {
 	Api api.API
+	Err error
 }
 
 func (f *FakeFactory) GetAPI() (api.API, error) {
-	return f.Api, nil
+	return f.Api, f.Err
 }
