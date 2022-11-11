cb30e4d4f2ef0ef65192680a27cf8517d3329f4b
Denis Shatilov <shatilov18@gmail.com>
2022-11-10 23:27:43 -0500

    ﻿AWS SQS Service Added
    Signed-off-by: Denis Shatilov <shatilov18@gmail.com>

---
+++ pkg/services/awssqs.go
@@ -0,0 +1,178 @@
+package services
+
+import (
+	"bytes"
+	"context"
+	"os"
+	texttemplate "text/template"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+)
+
+type AwsSqsNotification struct {
+	MessageAttributes map[string]string `json:"messageAttributes"`
+}
+
+type AwsSqsOptions struct {
+	Queue       string `json:"queue"`
+	Account     string `json:"account"`
+	Region      string `json:"region"`
+	EndpointUrl string `json:"endpointUrl,omitempty"`
+	AwsAccess
+}
+
+type AwsAccess struct {
+	Key    string `json:"key"`
+	Secret string `json:"secret"`
+}
+
+func NewAwsSqsService(opts AwsSqsOptions) NotificationService {
+	return &awsSqsService{opts: opts}
+}
+
+type awsSqsService struct {
+	opts AwsSqsOptions
+}
+
+func (s awsSqsService) Send(notif Notification, dest Destination) error {
+	options := s.setOptions()
+	cfg, err := config.LoadDefaultConfig(context.TODO(), options...)
+	if err != nil {
+		log.Fatalf("failed to load configuration, %v", err)
+	}
+
+	client := sqs.NewFromConfig(cfg)
+
+	queueUrl, err := GetQueueURL(context.TODO(), client, s.getQueueInput(dest))
+	if err != nil {
+		log.Error("Got an error getting the queue URL: ", err)
+		return err
+	}
+
+	sendMessage, err := SendMsg(context.TODO(), client, s.sendMessageInput(queueUrl.QueueUrl, notif))
+	if err != nil {
+		log.Error("Got an error sending the message: ", err)
+		return err
+	}
+	log.Debug("Message Sent with Id: ", *sendMessage.MessageId)
+
+	return nil
+}
+
+func (s awsSqsService) sendMessageInput(queueUrl *string, notif Notification) *sqs.SendMessageInput {
+	return &sqs.SendMessageInput{
+		QueueUrl:     queueUrl,
+		MessageBody:  aws.String(notif.Message),
+		DelaySeconds: 10,
+	}
+
+}
+func (s awsSqsService) getQueueInput(dest Destination) *sqs.GetQueueUrlInput {
+	result := &sqs.GetQueueUrlInput{}
+	result.QueueName = &s.opts.Queue
+
+	// Recipient in annotations takes precedent
+	if dest.Recipient != "" {
+		result.QueueName = &dest.Recipient
+	}
+
+	// Fill Account from configuration
+	if s.opts.Account != "" {
+		result.QueueOwnerAWSAccountId = &s.opts.Account
+	}
+	return result
+}
+
+func (s awsSqsService) setOptions() []func(*config.LoadOptions) error {
+	// Slice for AWS config options
+	var options []func(*config.LoadOptions) error
+
+	// When Credentials Are provided in service configuration - use them.
+	if (s.opts != AwsSqsOptions{} && s.opts.AwsAccess.Key != "" && s.opts.AwsAccess.Secret != "") {
+		options = append(options, config.WithCredentialsProvider(credentials.NewStaticCredentialsProvider(s.opts.AwsAccess.Key, s.opts.AwsAccess.Secret, "default")))
+	}
+
+	// Fill Region from configuration
+	if s.opts.Region != "" {
+		options = append(options, config.WithRegion(s.opts.Region))
+	}
+
+	// Useful for testing with localstack
+	if s.opts.EndpointUrl != "" {
+		endpointRegion := os.Getenv("AWS_DEFAULT_REGION")
+		if s.opts.Region != "" {
+			endpointRegion = s.opts.Region
+		}
+		customResolver := aws.EndpointResolverWithOptionsFunc(func(service, region string, options ...interface{}) (aws.Endpoint, error) {
+			if service == sqs.ServiceID {
+				return aws.Endpoint{
+					PartitionID:   "aws",
+					URL:           s.opts.EndpointUrl,
+					SigningRegion: endpointRegion,
+				}, nil
+			}
+			// returning EndpointNotFoundError will allow the service to fallback to it's default resolution
+			return aws.Endpoint{}, &aws.EndpointNotFoundError{}
+		})
+		options = append(options, config.WithEndpointResolverWithOptions(customResolver))
+	}
+	return options
+}
+
+func (n *AwsSqsNotification) GetTemplater(name string, f texttemplate.FuncMap) (Templater, error) {
+	return func(notification *Notification, vars map[string]interface{}) error {
+		if notification.AwsSqs == nil {
+			notification.AwsSqs = &AwsSqsNotification{}
+		}
+
+		if len(n.MessageAttributes) > 0 {
+			notification.AwsSqs.MessageAttributes = n.MessageAttributes
+			if err := notification.AwsSqs.parseMessageAttributes(name, f, vars); err != nil {
+				return err
+			}
+		}
+
+		return nil
+	}, nil
+}
+
+func (n *AwsSqsNotification) parseMessageAttributes(name string, f texttemplate.FuncMap, vars map[string]interface{}) error {
+	for k, v := range n.MessageAttributes {
+		var tempData bytes.Buffer
+
+		tmpl, err := texttemplate.New(name).Funcs(f).Parse(v)
+		if err != nil {
+			continue
+		}
+		if err := tmpl.Execute(&tempData, vars); err != nil {
+			return err
+		}
+		if val := tempData.String(); val != "" {
+			n.MessageAttributes[k] = val
+		}
+	}
+	return nil
+}
+
+type SQSSendMessageAPI interface {
+	GetQueueUrl(ctx context.Context,
+		params *sqs.GetQueueUrlInput,
+		optFns ...func(*sqs.Options)) (*sqs.GetQueueUrlOutput, error)
+
+	SendMessage(ctx context.Context,
+		params *sqs.SendMessageInput,
+		optFns ...func(*sqs.Options)) (*sqs.SendMessageOutput, error)
+}
+
+var GetQueueURL = func(c context.Context, api SQSSendMessageAPI, input *sqs.GetQueueUrlInput) (*sqs.GetQueueUrlOutput, error) {
+	return api.GetQueueUrl(c, input)
+}
+
+var SendMsg = func(c context.Context, api SQSSendMessageAPI, input *sqs.SendMessageInput) (*sqs.SendMessageOutput, error) {
+	return api.SendMessage(c, input)
+}
---
+++ pkg/services/awssqs_test.go
@@ -0,0 +1,252 @@
+package services
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
+	"text/template"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sqs"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetTemplater_AwsSqs(t *testing.T) {
+	n := Notification{
+		Message: "{{.message}}",
+		AwsSqs: &AwsSqsNotification{
+			MessageAttributes: map[string]string{
+				"attributeKey": "{{.messageAttributeValue}}",
+			},
+		},
+	}
+
+	templater, err := n.GetTemplater("", template.FuncMap{})
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	var notification Notification
+
+	err = templater(&notification, map[string]interface{}{
+		"message":               "abcdef",
+		"messageAttributeValue": "123456",
+	})
+
+	if !assert.NoError(t, err) {
+		return
+	}
+	assert.Equal(t, "abcdef", notification.Message)
+	assert.Equal(t, map[string]string{
+		"attributeKey": "123456",
+	}, notification.AwsSqs.MessageAttributes)
+}
+
+func TestSend_AwsSqs(t *testing.T) {
+
+	// Overriding methods inside, so service.Send could be called.
+	saveGetQueueURL := GetQueueURL
+	saveSendMsg := SendMsg
+
+	defer func() { SendMsg = saveSendMsg }()
+	defer func() { GetQueueURL = saveGetQueueURL }()
+
+	GetQueueURL = mockGetQueueURL("any", "")
+	SendMsg = mockSendMsg("1", "")
+
+	s := NewAwsSqsService(AwsSqsOptions{})
+
+	destination := Destination{Recipient: "test"}
+	notification := Notification{
+		Message: "Hello",
+		AwsSqs:  &AwsSqsNotification{},
+	}
+
+	if err := s.Send(notification, destination); err != nil {
+		assert.NoError(t, err)
+	}
+}
+
+func TestSendFail_AwsSqs(t *testing.T) {
+	s := NewTypedAwsSqsService(AwsSqsOptions{
+		Region: "us-east-1",
+		AwsAccess: AwsAccess{
+			Key:    "key",
+			Secret: "secret",
+		},
+		EndpointUrl: "localhost",
+		Account:     "123",
+	})
+
+	client := &fakeApi{"localhost", "1"}
+
+	destination := Destination{Recipient: "test"}
+	notification := Notification{
+		Message: "Hello",
+		AwsSqs:  &AwsSqsNotification{},
+	}
+	queueUrl, err := GetQueueURL(context.TODO(), client, s.getQueueInput(destination))
+	assert.NoError(t, err)
+
+	if _, err := SendMsg(context.TODO(), client, SendMessageInput(s, queueUrl.QueueUrl, notification)); err != nil {
+		assert.Error(t, err)
+	}
+}
+
+func TestSetOptions_AwsSqs(t *testing.T) {
+	s := NewTypedAwsSqsService(AwsSqsOptions{
+		Region: "us-east-1",
+		AwsAccess: AwsAccess{
+			Key:    "key",
+			Secret: "secret",
+		},
+		EndpointUrl: "localhost",
+	})
+
+	options := &config.LoadOptions{}
+	optionsF := SetOptions(s)
+
+	for _, f := range optionsF {
+		assert.NoError(t, f(options))
+	}
+	// Verify region properly set
+	assert.Equal(t, "us-east-1", options.Region)
+	// Get and Verify credentials from Provider
+	creds, _ := options.Credentials.Retrieve(context.TODO())
+	assert.Equal(t, s.opts.AwsAccess.Key, creds.AccessKeyID)
+	assert.Equal(t, s.opts.AwsAccess.Secret, creds.SecretAccessKey)
+	// Verify custom resolver is used
+	var resolverType aws.EndpointResolverWithOptionsFunc
+	assert.IsType(t, resolverType, options.EndpointResolverWithOptions)
+}
+
+func TestSetOptionsFromEnv_AwsSqs(t *testing.T) {
+	// Applying override via parameters instead of the ENV Variables
+	finalKey, finalSecret, finalRegion := "key", "secret", "us-east-1"
+
+	os.Setenv("AWS_ACCESS_KEY_ID", finalKey)
+	os.Setenv("AWS_SECRET_ACCESS_KEY", finalSecret)
+	os.Setenv("AWS_DEFAULT_REGION", finalRegion)
+
+	s := NewTypedAwsSqsService(AwsSqsOptions{})
+
+	options := SetOptions(s)
+	cfg, err := config.LoadDefaultConfig(context.TODO(), options...)
+	assert.NoError(t, err)
+
+	creds, _ := cfg.Credentials.Retrieve(context.TODO())
+
+	assert.Equal(t, finalKey, creds.AccessKeyID)
+	assert.Equal(t, finalSecret, creds.SecretAccessKey)
+	assert.Equal(t, finalRegion, cfg.Region)
+}
+
+func TestSetOptionsOverrideCredentials_AwsSqs(t *testing.T) {
+	os.Setenv("AWS_ACCESS_KEY_ID", "env_key")
+	os.Setenv("AWS_SECRET_ACCESS_KEY", "env_secret")
+	os.Setenv("AWS_DEFAULT_REGION", "us-east-2")
+
+	// Applying override via parameters instead of the ENV Variables
+	finalKey, finalSecret, finalRegion := "key", "secret", "us-east-1"
+
+	s := NewTypedAwsSqsService(AwsSqsOptions{
+		Region: finalRegion,
+		AwsAccess: AwsAccess{
+			Key:    finalKey,
+			Secret: finalSecret,
+		},
+	})
+
+	options := SetOptions(s)
+	cfg, err := config.LoadDefaultConfig(context.TODO(), options...)
+	assert.NoError(t, err)
+
+	creds, _ := cfg.Credentials.Retrieve(context.TODO())
+
+	assert.Equal(t, finalKey, creds.AccessKeyID)
+	assert.Equal(t, finalSecret, creds.SecretAccessKey)
+	assert.Equal(t, finalRegion, cfg.Region)
+}
+
+func TestSetOptionsCustomEndpointUrl_AwsSqs(t *testing.T) {
+	// Will be overridden
+	os.Setenv("AWS_DEFAULT_REGION", "us-east-2")
+
+	finalKey, finalSecret, finalRegion, finalEndpoint := "key", "secret", "us-east-1", "localhost"
+
+	s := NewTypedAwsSqsService(AwsSqsOptions{
+		Region: finalRegion,
+		AwsAccess: AwsAccess{
+			Key:    finalKey,
+			Secret: finalSecret,
+		},
+		EndpointUrl: finalEndpoint,
+	})
+
+	options := SetOptions(s)
+	cfg, err := config.LoadDefaultConfig(context.TODO(), options...)
+	assert.NoError(t, err)
+
+	creds, _ := cfg.Credentials.Retrieve(context.TODO())
+
+	assert.Equal(t, finalKey, creds.AccessKeyID)
+	assert.Equal(t, finalSecret, creds.SecretAccessKey)
+	assert.Equal(t, finalRegion, cfg.Region)
+
+	endpoint, _ := cfg.EndpointResolverWithOptions.ResolveEndpoint(sqs.ServiceID, finalRegion)
+	assert.Equal(t, finalEndpoint, endpoint.URL)
+
+}
+
+// Helpers
+var SetOptions = (*awsSqsService).setOptions
+var SendMessageInput = (*awsSqsService).sendMessageInput
+
+var NewTypedAwsSqsService = func(opts AwsSqsOptions) *awsSqsService {
+	return &awsSqsService{opts: opts}
+}
+
+type fakeApi struct {
+	Url       string
+	MessageId string
+}
+
+func (a fakeApi) SendMessage(ctx context.Context, params *sqs.SendMessageInput, optFns ...func(*sqs.Options)) (*sqs.SendMessageOutput, error) {
+	return &sqs.SendMessageOutput{
+		MessageId: aws.String(a.MessageId),
+	}, fmt.Errorf("%s", "fail scenario")
+}
+
+func (a fakeApi) GetQueueUrl(ctx context.Context, params *sqs.GetQueueUrlInput, optFns ...func(*sqs.Options)) (*sqs.GetQueueUrlOutput, error) {
+	var err error
+
+	return &sqs.GetQueueUrlOutput{
+		QueueUrl: aws.String(a.Url),
+	}, err
+}
+
+func mockSendMsg(messageId string, errorMsg string) func(c context.Context, api SQSSendMessageAPI, input *sqs.SendMessageInput) (*sqs.SendMessageOutput, error) {
+	return func(c context.Context, api SQSSendMessageAPI, input *sqs.SendMessageInput) (*sqs.SendMessageOutput, error) {
+		var err error
+		if errorMsg != "" {
+			err = fmt.Errorf("%s", errorMsg)
+		}
+		return &sqs.SendMessageOutput{
+			MessageId: aws.String(messageId),
+		}, err
+	}
+}
+
+func mockGetQueueURL(queueUrl string, errorMsg string) func(c context.Context, api SQSSendMessageAPI, input *sqs.GetQueueUrlInput) (*sqs.GetQueueUrlOutput, error) {
+	return func(c context.Context, api SQSSendMessageAPI, input *sqs.GetQueueUrlInput) (*sqs.GetQueueUrlOutput, error) {
+		var err error
+		if errorMsg != "" {
+			err = fmt.Errorf("%s", errorMsg)
+		}
+		return &sqs.GetQueueUrlOutput{
+			QueueUrl: aws.String(queueUrl),
+		}, err
+	}
+}
