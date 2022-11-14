2dfc477ea60dee0fcbcfb9e57382ab1974e5e608
Nan Yu <nanyu@google.com>
2022-11-14 20:19:45 +0000

    Make the Github's repoURL and revision paths configurable
    The Github service uses the hard-coded argo spec paths to extract the
    repo URL and synced revision. To make the notification engine generic,
    this commit makes the paths configurable. If they're not specified, it
    falls back to the argo API.
    
    Signed-off-by: Nan Yu <nanyu@google.com>

--- pkg/services/github_test.go
+++ pkg/services/github_test.go
@@ -58,3 +58,50 @@ func TestGetTemplater_GitHub(t *testing.T) {
 	assert.Equal(t, "continuous-delivery/argocd-notifications", notification.GitHub.Status.Label)
 	assert.Equal(t, "https://example.com/applications/argocd-notifications", notification.GitHub.Status.TargetURL)
 }
+
+func TestGetTemplater_GitHub_Custom_Resource(t *testing.T) {
+	n := Notification{
+		GitHub: &GitHubNotification{
+			RepoURLPath:  "{{.sync.spec.git.repo}}",
+			RevisionPath: "{{.sync.status.lastSyncedCommit}}",
+			Status: &GitHubStatus{
+				State: "synced",
+				Label: "continuous-delivery/{{.sync.metadata.name}}",
+			},
+		},
+	}
+	templater, err := n.GetTemplater("", template.FuncMap{})
+
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	var notification Notification
+	err = templater(&notification, map[string]interface{}{
+		"sync": map[string]interface{}{
+			"metadata": map[string]interface{}{
+				"name": "root-sync-test",
+			},
+			"spec": map[string]interface{}{
+				"git": map[string]interface{}{
+					"repo": "https://github.com/argoproj-labs/argocd-notifications.git",
+				},
+			},
+			"status": map[string]interface{}{
+				"lastSyncedCommit": "0123456789",
+			},
+		},
+	})
+
+	if !assert.NoError(t, err) {
+		return
+	}
+
+	assert.Equal(t, "{{.sync.spec.git.repo}}", notification.GitHub.RepoURLPath)
+	assert.Equal(t, "{{.sync.status.lastSyncedCommit}}", notification.GitHub.RevisionPath)
+	assert.Equal(t, "https://github.com/argoproj-labs/argocd-notifications.git", notification.GitHub.repoURL)
+	assert.Equal(t, "0123456789", notification.GitHub.revision)
+	assert.Equal(t, "synced", notification.GitHub.Status.State)
+	assert.Equal(t, "continuous-delivery/root-sync-test", notification.GitHub.Status.Label)
+	assert.Equal(t, "", notification.GitHub.Status.TargetURL)
+}
