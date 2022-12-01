eb0dea9ca0d9b7e9538689671a91356d6aebc86d
Vincent van Dam <vincentd@erg.verweg.com>
2022-12-01 01:26:13 +0100

    Improve error handling grafana (#77) (#119)
    Co-authored-by: pasha-codefresh <pavel@codefresh.io>

---
+++ pkg/services/grafana_test.go
@@ -0,0 +1,56 @@
+package services
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGrafana_SuccessfullySendsNotification(t *testing.T) {
+	var receivedHeaders http.Header
+	var receivedBody string
+	server := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		receivedHeaders = request.Header
+		data, err := ioutil.ReadAll(request.Body)
+		assert.NoError(t, err)
+		receivedBody = string(data)
+	}))
+	defer server.Close()
+
+	service := NewGrafanaService(GrafanaOptions{
+		ApiUrl:             server.URL,
+		ApiKey:             "something-secret-but-not-relevant-in-this-test",
+		InsecureSkipVerify: true,
+	})
+	err := service.Send(
+		Notification{
+			Message: "Annotation description",
+		}, Destination{Recipient: "tag1|tag2", Service: "test-service"})
+	assert.NoError(t, err)
+
+	assert.Contains(t, receivedBody, "tag1")
+	assert.Contains(t, receivedBody, "tag2")
+	assert.Contains(t, receivedBody, "Annotation description")
+	assert.Contains(t, receivedHeaders.Get("Authorization"), "Bearer")
+}
+
+func TestGrafana_UnSuccessfullySendsNotification(t *testing.T) {
+	server := httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		_, err := ioutil.ReadAll(request.Body)
+		assert.NoError(t, err)
+		writer.WriteHeader(http.StatusInternalServerError)
+	}))
+	defer server.Close()
+
+	service := NewGrafanaService(GrafanaOptions{
+		ApiUrl:             server.URL,
+		ApiKey:             "something-secret-but-not-relevant-in-this-test",
+		InsecureSkipVerify: true,
+	})
+	err := service.Send(
+		Notification{}, Destination{Recipient: "tag1|tag2", Service: "test-service"})
+	assert.Error(t, err)
+}
